830000ebde66e565c039499acd4ec05ef62f36a4
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-22 22:19:52 +0100

    add local-notifications plugin

--- app/main.js
+++ app/main.js
@@ -1,13 +1,14 @@
-import Vue from 'nativescript-vue'
-import App from './components/App'
+import Vue from "nativescript-vue";
+import App from "./components/App";
 
-import store from './store'
+require("nativescript-local-notifications");
+
+import store from "./store";
 
 // Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production')
-
+Vue.config.silent = TNS_ENV === "production";
 
 new Vue({
   store,
-  render: h => h('frame', [h(App)])
-}).$start()
+  render: h => h("frame", [h(App)])
+}).$start();
