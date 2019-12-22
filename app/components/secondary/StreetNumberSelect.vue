cf18cb731bdb0da009ad8b8ff521fc6841afd6c7
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-22 15:55:57 +0100

    validate number and get correct tour

--- app/components/secondary/StreetNumberSelect.vue
+++ app/components/secondary/StreetNumberSelect.vue
@@ -5,8 +5,9 @@
       <ActionItem @tap="onItemTap" ios.systemIcon="3" ios.position="right" text="Sichern" android.position="popup" />
     </ActionBar>
     <StackLayout class="page-bg">
-      <Label :text="streetName" class="text-center text-lg m-t-30"/>
+      <Label :text="streetName.name" class="text-center text-lg m-t-30"/>
       <TextField class="number-input bg-white border-gray-400 -rounded" v-model="streetNumber" @loaded="showKeyboard" hint="Hausnummer" maxLength="3" autocorrect="false" returnKeyType="done" keyboardType="phone"/>
+      <Label v-if="invalid" text="Bitte geben Sie eine gültige Hausnummer an." textWrap="true" class="text-center text-danger p-x-20 m-t-30"/>
     </StackLayout>
   </Page>
 </template>
@@ -18,7 +19,8 @@ export default {
   props: ['streetName'],
   data() {
     return {
-      streetNumber: ""
+      streetNumber: "",
+      invalid: false
     }
   },
   methods: {
@@ -33,15 +35,37 @@ export default {
       }
     },
     onItemTap(event) {
-      this.$store.commit('setStreetName', this.streetName)
-      this.$store.commit('setStreetNumber', this.streetNumber)
-      // create push notifications
-      this.$navigateTo(App, {
-        clearHistory: true,
-        props: {
-          selectedIndex: 3,
+      let numbers = this.streetName.numbers;
+      let numberOfDivisions = numbers.length;
+      
+      // only if valid
+      if (this.streetNumber <= numbers[numberOfDivisions - 1].nrBis) {
+        this.invalid = false;
+        
+        let tour;  
+        for (let i = 0; i < numberOfDivisions; i++) {
+          if (this.streetNumber >= numbers[i].nrVon && this.streetNumber <= numbers[i].nrBis) {
+            tour = numbers[i].tour;
+          }
         }
-      });
+
+        console.log(tour);
+        
+        this.$store.commit('setStreetName', this.streetName.name)
+        this.$store.commit('setStreetNumber', this.streetNumber)
+        this.$store.commit('setTour', tour);
+        
+        // create push notifications
+        
+        this.$navigateTo(App, {
+          clearHistory: true,
+          props: {
+            selectedIndex: 3,
+          }
+        });
+      } else {
+        this.invalid = true;
+      }
     }
   }
 }
