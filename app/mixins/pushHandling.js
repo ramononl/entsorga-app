f2da57cb19c6bf9d50eeda86134ce96b6345f162
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-22 22:22:42 +0100

    create push notifications mixin

---
+++ app/mixins/pushHandling.js
@@ -0,0 +1,159 @@
+import { alert } from "tns-core-modules/ui/dialogs";
+import { Color } from "tns-core-modules/color";
+import { LocalNotifications } from "nativescript-local-notifications";
+
+import dates from "~/assets/dates.json";
+
+export const pushHandling = {
+  data() {
+    return {
+      dates: dates.tours
+    };
+  },
+  created() {
+    LocalNotifications.addOnMessageReceivedCallback(notificationData => {
+      this.message =
+        "Notification received: " + JSON.stringify(notificationData);
+    });
+  },
+  methods: {
+    onTapHasPermission() {
+      LocalNotifications.hasPermission().then(granted => {
+        alert({
+          title: "Permission granted?",
+          message: granted ? "YES" : "NO",
+          okButtonText: "OK"
+        });
+      });
+    },
+    createNotifications() {
+      let days = ["So.", "Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa."];
+      let months = [
+        "Januar",
+        "Februar",
+        "März",
+        "April",
+        "Mai",
+        "Juni",
+        "Juli",
+        "August",
+        "September",
+        "Oktober",
+        "November",
+        "Dezember"
+      ];
+      let tour = this.$store.state.user.tour;
+
+      if (this.$store.state.user.pushPaper) {
+        var paperDates = this.dates.paper[tour];
+        var paperDateObjects = [];
+        for (let i = 0; i < paperDates.length; i++) {
+          let splittedDate = paperDates[i].split("-");
+          paperDateObjects[i] = {
+            year: parseInt(splittedDate[0]),
+            month: parseInt(splittedDate[1]) - 1,
+            day: parseInt(splittedDate[2]),
+            weekday: days[new Date(paperDates[i]).getDay()],
+            monthName: months[new Date(paperDates[i]).getMonth()]
+          };
+        }
+      }
+
+      if (this.$store.state.user.pushCarton) {
+        var cartonDates = this.dates.carton[tour];
+        var cartonDateObjects = [];
+        for (let i = 0; i < cartonDates.length; i++) {
+          let splittedDate = cartonDates[i].split("-");
+          cartonDateObjects[i] = {
+            year: parseInt(splittedDate[0]),
+            month: parseInt(splittedDate[1]) - 1,
+            day: parseInt(splittedDate[2]),
+            weekday: days[new Date(cartonDates[i]).getDay()],
+            monthName: months[new Date(cartonDates[i]).getMonth()]
+          };
+        }
+      }
+
+      let hour = this.$store.state.user.pushTime.hour;
+      let minute = this.$store.state.user.pushTime.minute;
+      let numberOfDaysBefore = this.$store.state.user.pushDay;
+
+      let notificationArr = [];
+      let id = 1;
+
+      if (this.$store.state.user.pushPaper) {
+        for (let i = 0; i < paperDateObjects.length; i++) {
+          let notification = {
+            id: id,
+            title: `Papiersammlung am ${paperDateObjects[i].weekday} ${paperDateObjects[i].day}. ${paperDateObjects[i].monthName}`,
+            body:
+              "Demnächst wird an Ihrer Adresse Papier gesammelt. Nutzen Sie die Gelegenheit, Ihr Altpapier gebündelt bereitzustellen.",
+            image: "https://entsorga.ramon.onl/assets/paper.jpg",
+            forceShowWhenInForeground: true,
+            // at: new Date(new Date().getTime() + 5 * 1000)
+            at: new Date(
+              paperDateObjects[i].year,
+              paperDateObjects[i].month,
+              paperDateObjects[i].day - numberOfDaysBefore,
+              hour,
+              minute
+            )
+          };
+          notificationArr.push(notification);
+          id++;
+        }
+      }
+
+      if (this.$store.state.user.pushCarton) {
+        for (let i = 0; i < cartonDateObjects.length; i++) {
+          let notification = {
+            id: id,
+            title: `Kartonsammlung am ${cartonDateObjects[i].weekday} ${cartonDateObjects[i].day}. ${cartonDateObjects[i].monthName}`,
+            body:
+              "Demnächst wird an Ihrer Adresse Karton gesammelt. Nutzen Sie die Gelegenheit, Ihren Altkarton gebündelt bereitzustellen.",
+            image: "https://entsorga.ramon.onl/assets/carton.jpg",
+            forceShowWhenInForeground: true,
+            // at: new Date(new Date().getTime() + 5 * 1000)
+            at: new Date(
+              cartonDateObjects[i].year,
+              cartonDateObjects[i].month,
+              cartonDateObjects[i].day - numberOfDaysBefore,
+              hour,
+              minute
+            )
+          };
+          notificationArr.push(notification);
+          id++;
+        }
+      }
+
+      // clear all previous notifications, then create updated notifications
+      LocalNotifications.cancelAll()
+        .then(() => {
+          console.log("all canceled");
+          if (notificationArr && notificationArr.length) {
+            LocalNotifications.schedule(notificationArr)
+              .then(() => {
+                alert({
+                  title: "Notification scheduled",
+                  message: "ID: 1",
+                  okButtonText: "OK, thanks"
+                });
+              })
+              .catch(error => console.log("doSchedule error: " + error));
+            // console.log("created notifications");
+          } else {
+            console.log("no notifications to create");
+          }
+        })
+        .catch(error => console.log("doCancelAll error: " + error));
+    },
+    cancelAllNotifications() {
+      LocalNotifications.cancelAll()
+        .then(() => {
+          console.log("all canceled");
+        })
+        .catch(error => console.log("doCancelAll error: " + error));
+    }
+  }
+};
