e5d8e60eb6e730a6e84ba203d9742f7decbe28f5
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-23 18:44:02 +0100

    add setup option

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 import Vue from "nativescript-vue";
 import App from "./components/App";
+import Setup from "./components/Setup";
 
 require("nativescript-local-notifications");
 
@@ -8,7 +9,15 @@ import store from "./store";
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = TNS_ENV === "production";
 
+let setupDone = store.state.setupDone;
+
 new Vue({
   store,
-  render: h => h("frame", [h(App)])
+  render(h) {
+    if (!setupDone) {
+      return h("frame", [h(Setup)]);
+    } else {
+      return h("frame", [h(App)]);
+    }
+  }
 }).$start();
