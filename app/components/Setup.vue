3b2b80daa639774badf36a41c0f97a8502d8e5df
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-23 18:47:02 +0100

    add setup screen

---
+++ app/components/Setup.vue
@@ -0,0 +1,26 @@
+<template>
+  <Page class="page-bg">
+    <DockLayout stretchLastChild="true">
+      <Label text="entsorga" dock="top" class="app-name border-b border-white-transparent"/>
+      <Label text="content"/>
+    </DockLayout>
+  </Page>
+</template>
+
+<script>
+import * as ApplicationSettings from "application-settings";
+
+export default {
+  name: 'Main',
+  mounted() {
+    this.$store.subscribe((mutations, state) => {
+      ApplicationSettings.setString("store", JSON.stringify(state));
+    });
+    this.$store.commit("load");
+  }
+}
+</script>
+
+<style>
+
+</style>
