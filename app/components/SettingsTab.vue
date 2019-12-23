7a088c45708c4a03d42c77f6b245f4ae77869503
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-23 22:05:42 +0100

    add reset button and method

--- app/components/SettingsTab.vue
+++ app/components/SettingsTab.vue
@@ -1,43 +1,45 @@
 <template>
   <ScrollView class="w-full">
     <FlexboxLayout flexDirection="column" justifyContent="center" class="page-padding">
-        <Label text="Einstellungen" class="h1 p-x-20"/>
-        <FlexboxLayout justifyContent="space-between" alignItems="center" class="action-item action-item-first rounded-sm" @tap="goToStreetSelect">
-          <Label text="Adresse" class="text-primary font-bold" flexGrow="0"/>
-          <FlexboxLayout justifyContent="flex-end" alignItems="center" flexGrow="1">
-            <Label :text="userStreetName + ' ' + userStreetNumber" class="text-secondary text-right"/>
-            <StackLayout>
-              <Image src="res://appicons/icon-cheveron" stretch="none" class="cheveron"/>
-            </StackLayout>
-          </FlexboxLayout>
+      <Label text="Einstellungen" class="h1 p-x-20"/>
+      <FlexboxLayout justifyContent="space-between" alignItems="center" class="action-item action-item-first rounded-sm" @tap="goToStreetSelect">
+        <Label text="Adresse" class="text-primary font-bold" flexGrow="0"/>
+        <FlexboxLayout justifyContent="flex-end" alignItems="center" flexGrow="1">
+          <Label :text="userStreetName + ' ' + userStreetNumber" class="text-secondary text-right"/>
+          <StackLayout>
+            <Image src="res://appicons/icon-cheveron" stretch="none" class="cheveron"/>
+          </StackLayout>
         </FlexboxLayout>
-
-        <Label text="Mitteilungen" class="section-description"/>
-        <FlexboxLayout justifyContent="space-between" alignItems="center" alignContent="center" :class="[userPushNotifications ? 'rounded-t-sm' : 'rounded-sm']" class="action-item action-item-first action-item-switch">
-          <Label text="Mitteilungen anzeigen" class="text-primary font-bold"/>
-          <Switch class="switch" :checked="userPushNotifications" @tap="switchReady = true" @checkedChange="changePush($event, 'setPushNotifications')"/>
-        </FlexboxLayout>
-        <FlexboxLayout v-if="userPushNotifications" justifyContent="space-between" alignItems="center" class="action-item border-gray-800 border-t action-item-switch">
-          <Label text="Mitteilungen für Papier" class="text-primary font-bold"/>
-          <Switch class="switch" v-if="userPushNotifications" @tap="switchReady = true" :checked="userPushPaper" @checkedChange="changePush($event, 'setPushPaper')"/>
-        </FlexboxLayout>
-        <FlexboxLayout v-if="userPushNotifications" justifyContent="space-between" alignItems="center" class="action-item action-item-switch border-gray-800 border-t rounded-b-sm">
-          <Label text="Mitteilungen für Karton" class="text-primary font-bold"/>
-          <Switch class="switch" v-if="userPushNotifications" @tap="switchReady = true" :checked="userPushCarton" @checkedChange="changePush($event, 'setPushCarton')"/>
-        </FlexboxLayout>
-        
-        <Label text="Zeitpunkt der Mitteilungen" class="section-description"/>
-        <FlexboxLayout justifyContent="space-between" alignItems="center" class="action-item action-item-first rounded-sm" @tap="showPushTimeSelect">
-          <Label text="Termin" class="text-primary font-bold" flexGrow="0"/>
-          <FlexboxLayout justifyContent="flex-end" alignItems="center" flexGrow="1">
-            <Label :text="pushDayList[pushDay] + ' um ' + pushTime + ' Uhr'" class="text-secondary text-right"/>
-            <StackLayout>
-              <Image src="res://appicons/icon-cheveron" stretch="none" class="cheveron"/>
-            </StackLayout>
-          </FlexboxLayout>
-        </FlexboxLayout>
-        
       </FlexboxLayout>
+
+      <Label text="Mitteilungen" class="section-description"/>
+      <FlexboxLayout justifyContent="space-between" alignItems="center" alignContent="center" :class="[userPushNotifications ? 'rounded-t-sm' : 'rounded-sm']" class="action-item action-item-first action-item-switch">
+        <Label text="Mitteilungen anzeigen" class="text-primary font-bold"/>
+        <Switch class="switch" :checked="userPushNotifications" @tap="switchReady = true" @checkedChange="changePush($event, 'setPushNotifications')"/>
+      </FlexboxLayout>
+      <FlexboxLayout v-if="userPushNotifications" justifyContent="space-between" alignItems="center" class="action-item border-gray-800 border-t action-item-switch">
+        <Label text="Mitteilungen für Papier" class="text-primary font-bold"/>
+        <Switch class="switch" v-if="userPushNotifications" @tap="switchReady = true" :checked="userPushPaper" @checkedChange="changePush($event, 'setPushPaper')"/>
+      </FlexboxLayout>
+      <FlexboxLayout v-if="userPushNotifications" justifyContent="space-between" alignItems="center" class="action-item action-item-switch border-gray-800 border-t rounded-b-sm">
+        <Label text="Mitteilungen für Karton" class="text-primary font-bold"/>
+        <Switch class="switch" v-if="userPushNotifications" @tap="switchReady = true" :checked="userPushCarton" @checkedChange="changePush($event, 'setPushCarton')"/>
+      </FlexboxLayout>
+      
+      <Label text="Zeitpunkt der Mitteilungen" class="section-description"/>
+      <FlexboxLayout justifyContent="space-between" alignItems="center" class="action-item action-item-first rounded-sm" @tap="showPushTimeSelect">
+        <Label text="Termin" class="text-primary font-bold" flexGrow="0"/>
+        <FlexboxLayout justifyContent="flex-end" alignItems="center" flexGrow="1">
+          <Label :text="pushDayList[pushDay] + ' um ' + pushTime + ' Uhr'" class="text-secondary text-right"/>
+          <StackLayout>
+            <Image src="res://appicons/icon-cheveron" stretch="none" class="cheveron"/>
+          </StackLayout>
+        </FlexboxLayout>
+      </FlexboxLayout>
+
+      <Label text="App zurücksetzen" @tap="resetApp" class="text-pink-100 text-center m-t-20"/>
+        
+    </FlexboxLayout>
   </ScrollView>
 </template>
 
@@ -107,6 +109,12 @@ export default {
         }
         this.createNotifications();
       }
+    },
+    resetApp() {
+      this.$store.commit('setSetupDone', false);
+      // remove all notifications
+      // clear store
+      // clear application settings
     }
   }
 }
