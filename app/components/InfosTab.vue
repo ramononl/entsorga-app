6a96b5c77ec74e0b9e667e787c544d24962773c2
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-23 18:46:18 +0100

    add recycle infos

--- app/components/InfosTab.vue
+++ app/components/InfosTab.vue
@@ -1,14 +1,24 @@
 <template>
-  <ScrollView class="page-bg">
-    <FlexboxLayout flexDirection="column" justifyContent="center">
-      <Label text="Infos" />
+  <ScrollView class="w-full">
+    <FlexboxLayout flexDirection="column" justifyContent="center" class="page-padding">
+      <Label text="Wo kann ich was entsorgen?" textWrap="true" class="h1 p-x-20"/>
+      <StackLayout v-for="(type, index) in infos" :key="index" class="bg-white-transparent rounded-sm p-x-20 p-y-10 m-t-10">
+        <Label :text="type.name" textWrap="true" class="text-primary font-bold"/>
+        <Label :text="type.desc" textWrap="true" class="text-secondary"/>
+      </StackLayout>
     </FlexboxLayout>
   </ScrollView>
 </template>
 
 <script>
-export default {
+import infos from "~/assets/infos.json";
 
+export default {
+  data() {
+    return {
+      infos: infos.types
+    }
+  }
 }
 </script>
 
