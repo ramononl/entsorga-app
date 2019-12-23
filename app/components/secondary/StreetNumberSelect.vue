396d69fbaf0e339f48acd5f81cd30ccf50b71fdf
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-23 18:47:37 +0100

    style input

--- app/components/secondary/StreetNumberSelect.vue
+++ app/components/secondary/StreetNumberSelect.vue
@@ -1,37 +1,57 @@
 <template>
   <Page>
     <ActionBar title="Strasse wählen">
-      <NavigationButton text=""/>
-      <ActionItem @tap="onItemTap" ios.systemIcon="3" ios.position="right" text="Sichern" android.position="popup" />
+      <NavigationButton text />
+      <ActionItem
+        @tap="onItemTap"
+        ios.systemIcon="3"
+        ios.position="right"
+        text="Sichern"
+        android.position="popup"
+      />
     </ActionBar>
-    <StackLayout class="page-bg">
-      <Label :text="streetName.name" class="text-center text-lg m-t-30"/>
-      <TextField class="number-input bg-white border-gray-400 -rounded" v-model="streetNumber" @loaded="showKeyboard" hint="Hausnummer" maxLength="3" autocorrect="false" returnKeyType="done" keyboardType="phone"/>
-      <Label v-if="invalid" text="Bitte geben Sie eine gültige Hausnummer an." textWrap="true" class="text-center text-danger p-x-20 m-t-30"/>
+    <StackLayout class="page-bg-secondary">
+      <Label :text="streetName.name" class="text-center text-lg m-t-30 text-gray-600" />
+      <TextField
+        class="bg-white border-gray-800 border rounded-sm m-x-20 m-y-10 p-x-20 p-y-30 text-center text-20"
+        v-model="streetNumber"
+        @loaded="showKeyboard"
+        hint="Hausnummer"
+        maxlength="3"
+        autocorrect="false"
+        returnKeyType="done"
+        keyboardType="phone"
+      />
+      <Label
+        v-if="invalid"
+        text="Bitte geben Sie eine gültige Hausnummer an."
+        textWrap="true"
+        class="text-center text-danger p-x-20 m-t-30"
+      />
     </StackLayout>
   </Page>
 </template>
 
 <script>
 import App from "../App";
-import { pushHandling } from '../../mixins/pushHandling';
+import { pushHandling } from "../../mixins/pushHandling";
 
 export default {
-  props: ['streetName'],
+  props: ["streetName"],
   data() {
     return {
       streetNumber: "",
       invalid: false
-    }
+    };
   },
   mixins: [pushHandling],
   methods: {
     showKeyboard({ object }) {
       if (object.android) {
         setTimeout(() => {
-        object.focus();
-        utils.ad.showSoftInput(object);
-      }, 10);
+          object.focus();
+          utils.ad.showSoftInput(object);
+        }, 10);
       } else {
         object.focus();
       }
@@ -39,31 +59,35 @@ export default {
     onItemTap(event) {
       let numbers = this.streetName.numbers;
       let numberOfDivisions = numbers.length;
-      
+
       // only if valid
       if (this.streetNumber <= numbers[numberOfDivisions - 1].nrBis) {
         this.invalid = false;
-        
-        let tour;  
+
+        let tour;
         for (let i = 0; i < numberOfDivisions; i++) {
-          if (this.streetNumber >= numbers[i].nrVon && this.streetNumber <= numbers[i].nrBis) {
+          if (
+            this.streetNumber >= numbers[i].nrVon &&
+            this.streetNumber <= numbers[i].nrBis
+          ) {
             tour = numbers[i].tour;
+            break ;
           }
         }
 
         console.log(tour);
-        
-        this.$store.commit('setStreetName', this.streetName.name)
-        this.$store.commit('setStreetNumber', this.streetNumber)
-        this.$store.commit('setTour', tour);
-        
+
+        this.$store.commit("setStreetName", this.streetName.name);
+        this.$store.commit("setStreetNumber", this.streetNumber);
+        this.$store.commit("setTour", tour);
+
         // create push notifications
         this.createNotifications();
-        
+
         this.$navigateTo(App, {
           clearHistory: true,
           props: {
-            selectedIndex: 3,
+            selectedIndex: 3
           }
         });
       } else {
@@ -71,16 +95,9 @@ export default {
       }
     }
   }
-}
+};
 </script>
 
-<style scoped>
-  .number-input {
-    margin: 30 20 0;
-    border-width: 1px;
-    padding: 20 30;
-    border-radius: 8;
-    font-size: 20;
-    text-align: center;
-  }
+<style>
+
 </style>
