20111b18abcca4fef91c5428f949f391f991415c
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-23 22:04:07 +0100

    add nativescript-carousel

--- app/main.js
+++ app/main.js
@@ -3,21 +3,21 @@ import App from "./components/App";
 import Setup from "./components/Setup";
 
 require("nativescript-local-notifications");
+Vue.registerElement(
+  "Carousel",
+  () => require("nativescript-carousel").Carousel
+);
+Vue.registerElement(
+  "CarouselItem",
+  () => require("nativescript-carousel").CarouselItem
+);
 
 import store from "./store";
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = TNS_ENV === "production";
 
-let setupDone = store.state.setupDone;
-
 new Vue({
   store,
-  render(h) {
-    if (!setupDone) {
-      return h("frame", [h(Setup)]);
-    } else {
-      return h("frame", [h(App)]);
-    }
-  }
+  render: h => h("Frame", [h(App)])
 }).$start();
