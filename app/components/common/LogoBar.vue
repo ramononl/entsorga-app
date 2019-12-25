c9f5b63849273284257b650c7145fa56435d5e85
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-25 20:57:39 +0100

    add icon

--- app/components/common/LogoBar.vue
+++ app/components/common/LogoBar.vue
@@ -1,3 +1,6 @@
 <template>
-  <Label text="entsorga" dock="top" class="app-name border-b border-white-transparent"/>
+  <FlexboxLayout dock="top" alignItems="center" justifyContent="center" height="50" class="w-full m-t-5 p-b-10 border-b border-white-transparent">
+    <Image src="res://entsorga" stretch="none"/>
+    <Label text="entsorga" class="app-name m-l-10"/>
+  </FlexboxLayout>
 </template>
