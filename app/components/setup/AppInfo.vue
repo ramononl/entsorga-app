2ecfb63ca7b078a59748c776b9e14b2d6bfee59a
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-25 20:57:49 +0100

    add setup steps

---
+++ app/components/setup/AppInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <StackLayout verticalAlignment="middle" class="h-full">
+    <Carousel height="200" width="100%" pageChanged="myChangeEvent" pageTapped="mySelectedEvent" indicatorColor="#ffffff" finite="true" bounce="true" showIndicator="true" verticalAlignment="top" android:indicatorAnimation="swap" color="white">
+      <CarouselItem id="slide1" verticalAlignment="middle" horizontalAlignment="center">
+        <Image src="res://startseite" stretch="none" horizontalAlignment="center"/>
+        <Label text="Slide 1" class="text-center"/>
+      </CarouselItem>
+      <CarouselItem id="slide2" verticalAlignment="middle" horizontalAlignment="center">
+        <Image src="res://startseite" stretch="none" horizontalAlignment="center"/>
+        <Label text="Slide 2" class="text-center"/>
+      </CarouselItem>
+      <CarouselItem id="slide3" verticalAlignment="middle" horizontalAlignment="center">
+        <Image src="res://startseite" stretch="none" horizontalAlignment="center"/>
+        <Label text="Slide 3" class="text-center"/>
+      </CarouselItem>
+      <CarouselItem id="slide4" verticalAlignment="middle" horizontalAlignment="center">
+        <Image src="res://startseite" stretch="none" horizontalAlignment="center"/>
+        <Label text="Slide 4" class="text-center"/>
+      </CarouselItem>
+    </Carousel>
+    <Button text="Jetzt loslegen" @tap="onButtonTap" class="m-t-30 w-full"/>
+  </StackLayout>
+</template>
+
+<script>
+export default {
+  methods: {
+    onButtonTap() {
+      this.$emit("nextstep", "SetupStreetSelect")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
