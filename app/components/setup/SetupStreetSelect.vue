11f520a21ac5a0432e7081ee32b7e04e0547dd93
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-27 16:54:06 +0100

    fix classes

--- app/components/setup/SetupStreetSelect.vue
+++ app/components/setup/SetupStreetSelect.vue
@@ -1,9 +1,9 @@
 <template>
-  <StackLayout class="h-full">
+  <StackLayout class="h-full p-x-20">
     <Label textWrap="true" text="An welcher Strasse wohnen Sie?" class="h1 p-x-20"/>
-    <TextField v-model="searchBar" hint="Strasse suchen..." returnKeyType="search" editable="true" autocorrect="false" class="bg-white-transparent w-full m-t-20 p-x-20 p-y-10 text-20 rounded-sm"/>
-    <StackLayout class="bg-white-transparent rounded-sm m-t-10">
-      <ListView for="streetName in filteredNames" @itemTap="onItemTap" separatorColor="gray">
+    <TextField v-model="searchBar" hint="Strasse suchen..." returnKeyType="search" editable="true" autocorrect="false" class="bg-white-transparent w-full m-t-20 m-b-10 p-x-20 p-y-10 text-md rounded-sm"/>
+    <StackLayout class="bg-white-transparent rounded-sm h-full">
+      <ListView for="streetName in filteredNames" @itemTap="onItemTap" separatorColor="gray" class="h-full">
         <v-template>
           <GridLayout height="45">
             <Label :text="streetName.name" class="p-20"/>
