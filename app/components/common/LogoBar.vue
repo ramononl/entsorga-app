39ab05ef085ef24002aa10898fca241ca19c351e
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-27 16:51:42 +0100

    add border around image

--- app/components/common/LogoBar.vue
+++ app/components/common/LogoBar.vue
@@ -1,6 +1,6 @@
 <template>
   <FlexboxLayout dock="top" alignItems="center" justifyContent="center" height="50" class="w-full m-t-5 p-b-10 border-b border-white-transparent">
-    <Image src="res://entsorga" stretch="none"/>
+    <Image src="res://entsorga" stretch="none" class="border border-white-transparent-50 rounded-sm"/>
     <Label text="entsorga" class="app-name m-l-10"/>
   </FlexboxLayout>
 </template>
