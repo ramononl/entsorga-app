938b69079a8c9ef39b3536205c8fa872014a7cc6
ramononl <48051529+ramononl@users.noreply.github.com>
2019-12-27 16:47:47 +0100

    add nativescript-mapbox

--- app/main.js
+++ app/main.js
@@ -1,8 +1,12 @@
 import Vue from "nativescript-vue";
+// import application-settings
 import * as ApplicationSettings from "application-settings";
+// import custom routes
 import routes from "./routes";
-
+// require loca-notifications plugin
 require("nativescript-local-notifications");
+
+// create custom vue elements
 Vue.registerElement(
   "Carousel",
   () => require("nativescript-carousel").Carousel
@@ -11,9 +15,10 @@ Vue.registerElement(
   "CarouselItem",
   () => require("nativescript-carousel").CarouselItem
 );
+Vue.registerElement("Mapbox", () => require("nativescript-mapbox").MapboxView);
 
+// check if initial app setup is done
 let setupDone = false;
-
 if (ApplicationSettings.hasKey("store")) {
   let settingsObj = JSON.parse(ApplicationSettings.getString("store"));
   let checkSetupDone = settingsObj.setupDone;
@@ -22,11 +27,13 @@ if (ApplicationSettings.hasKey("store")) {
   }
 }
 
+// import vuex store
 import store from "./store";
 
-// Prints Vue logs when --env.production is *NOT* set while building
+// print vue logs when --env.production is NOT set while building
 Vue.config.silent = TNS_ENV === "production";
 
+// create vue instance, render route based on "setupDone"
 new Vue({
   store,
   render: h => h("frame", [h(setupDone ? routes.app : routes.setup)])
